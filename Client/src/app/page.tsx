4a151aaa883c78dcf9a2b87ff20c2f067bfd9a6b
Steven Spencer <stevenistotallyawesome@gmail.com>
2025-06-29 18:46:34 -0400

    Added all light mode support.

--- Client/src/app/page.tsx
+++ Client/src/app/page.tsx
@@ -21,7 +21,7 @@ export default function HomePage() {
     <div className="flex flex-col">
 
       <section>
-        <div className="fixed top-0 w-full h-fit z-[-1] bg-[#ffffff] dark:bg-[#171717] 
+        <div className="fixed top-0 w-full h-fit z-[-1] bg-[#F5F5F5] dark:bg-[#171717] 
         transition-colors duration-300 ease-in-out
         flex flex-col gap-10 items-center justify-center pb-30" >
           <div className=" relative w-[90vw] max-w-[640px] aspect-[1/1]">
@@ -52,7 +52,7 @@ export default function HomePage() {
       {/* Home Page Header */}
       <section id="home">
         <Header data={{ title: "Tupah", subtext: "Unfiltered thoughts with occasional genius." }}
-          className="flex sm:justify-between justify-center bg-[#272727] dark:bg-[#1c1c1c] p-5 h-32 w-full z-2"
+          className="flex sm:justify-between justify-center bg-[#eff1f1] shadow-sm dark:bg-[#1c1c1c] p-5 h-32 w-full z-2"
         />
 
       </section>
@@ -62,15 +62,15 @@ export default function HomePage() {
         <div className="flex flex-col md:flex-row content-between py-30 text-center">
           <div className="flex flex-col flex-1 items-start justify-baseline p-10">
             <div>
-              <h1 className="text-[#9379cc] font-bold text-2xl">What's here</h1>
-              <p className="text-white outline-black w-44 overflow-hidden">A bunch of blog posts I have started to accumulate. I post cool stuff sometimes.
+              <h1 className="text-[#1272CC] dark:text-[#9379cc] font-bold text-2xl text-shadow-2xs">What's here</h1>
+              <p className=" text-gray-800 dark:text-white outline-black w-44 overflow-hidden">A bunch of blog posts I have started to accumulate. I post cool stuff sometimes.
               </p>
             </div>
           </div>
           <div className="flex flex-col flex-1 items-end justify-end p-10 text-center">
             <div>
-              <h1 className="text-[#9379cc] font-bold text-2xl text-center">Why it's here</h1>
-              <p className="text-white outline-black w-44 overflow-hidden">I made this blog because I have nothing better do with myself and I enjoy it.
+              <h1 className="text-[#1272CC] dark:text-[#9379cc] font-bold text-2xl text-center text-shadow-2xs">Why it's here</h1>
+              <p className="text-gray-800 dark:text-white outline-black w-44 overflow-hidden">I made this blog because I have nothing better do with myself and I enjoy it.
               </p>
             </div>
           </div>
@@ -79,22 +79,24 @@ export default function HomePage() {
 
       {/* What I post section */}
       <section id="posts">
-        <div className="flex flex-col p-10 gap-5 w-full justify-center items-center bg-[#ffffff] dark:bg-[#0e0e0e] text-white">
-          <h2 className="text-4xl font-bold text-center">What do I post here?</h2>
+        <div className="flex flex-col p-10 gap-5 w-full justify-center items-center bg-[#f0f0f0] dark:bg-[#0e0e0e] shadow-xl shodow-indigo-500/50 text-white">
+          <h2 className="text-4xl font-bold text-center text-gray-700 dark:text-white">What do I post here?</h2>
           <div className="flex flex-col md:flex-row gap-5 justify-center items-center">
-            <div>
-              <h3 className="text-2xl font-semibold text-[#9379cc] underline text-center md:text-left mb-2">Gaming</h3>
-              <p>I've been playing Marvel Rivals and I like keeping my pages updated with the newest Marvel
+            <div className="flex flex-col items-center">
+              <h3 className="text-2xl font-semibold text-[#1272CC] dark:text-[#9379cc] underline text-center md:text-left mb-2">Gaming</h3>
+              <p className="text-1xl font-normal text-gray-800 dark:text-white">I've been playing Marvel Rivals and I like keeping my pages updated with the newest Marvel
                 related content. I used to play LoL, Overwatch, Old School RuneScape, and so many other titles
                 I could go on for days.</p>
             </div>
-            <div>
-              <h3 className="text-2xl font-semibold text-[#9379cc] underline text-center md:text-left mb-2">Anime</h3>
-              <p>I enjoy watching seasonal anime and posting my favorites. I post about the worst and best anime of each season.</p>
+            <div className="relative bg-black rounded-4xl"></div>
+            <div className="flex flex-col items-center">
+              <h3 className="text-2xl font-semibold text-[#1272CC] dark:text-[#9379cc] underline text-center md:text-left mb-2">Anime</h3>
+              <p className="text-gray-800 dark:text-white">I enjoy watching seasonal anime and posting my favorites. I post about the worst and best anime of each season.</p>
             </div>
-            <div>
-              <h3 className="text-2xl font-semibold text-[#9379cc] underline text-center md:text-left mb-2">Tech Support</h3>
-              <p>I'm at Michigan State University studying for my Bachelor's in Computer Science, so I know might know something about computers.
+
+            <div className="flex flex-col items-center">
+              <h3 className="text-2xl font-semibold text-[#1272CC] dark:text-[#9379cc] underline text-center md:text-left mb-2">Tech Support</h3>
+              <p className="text-gray-800 dark:text-white">I'm at Michigan State University studying for my Bachelor's in Computer Science, so I know might know something about computers.
                 I have always wanted to start a page where I could let people know useful tutorials or helpful tips,
                 this is that dream realized.</p>
             </div>
@@ -102,15 +104,14 @@ export default function HomePage() {
         </div>
 
         {/* Rotating Blog Posts */}
-        <div className="flex items-center justify-center w-full h-fit bg-[#121212] py-5">
-          <RotatingIcons data={{ icons: rotating_icons }} className="bg-[#9379cc] rounded-4xl m-5" />
+        <div className="flex items-center justify-center w-full h-fit bg-gray-200 dark:bg-[#121212] py-5">
+          <RotatingIcons data={{ icons: rotating_icons }} className="bg-[#5994cc] dark:bg-[#9379cc] rounded-4xl m-5" />
         </div>
       </section>
 
       {/* Brook Image */}
       <section id="brook">
-        <div className="flex flex-col gap-1 justify-center items-center h-fit w-full bg-amber-50 p-5">
-
+        <div className="flex flex-col gap-1 justify-center items-center h-fit w-full bg-gray-400 dark:bg-amber-50 p-5">
           <p className="italic text-2xl p-3">"Death leaves nothing behind."</p>
           {/* Responsive Image Container */}
           <div className="relative lg:w-32 lg:h-32 md:w-24 md:h-24 w-16 h-16">
