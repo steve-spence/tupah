55276c4ed92d72d494ab0041b2ac8cbdfd1a3b87
spenc426 <stevenistotallyawesome@gmail.com>
2025-08-18 20:40:32 -0400

    trying to fix the redirect url

--- Client/next.config.ts
+++ Client/next.config.ts
@@ -5,6 +5,21 @@ const nextConfig: NextConfig = {
   /* config options here */
   // pageExtensions: ['mdx'],
   transpilePackages: ['next-mdx-remote'],
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'tupah.me',
+          },
+        ],
+        destination: 'https://www.tupah.me/:path*',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 // Add markdown plugins here !!!! Turbopack does not support .md files yet !!!!!
