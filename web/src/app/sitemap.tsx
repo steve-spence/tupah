f8e6625c41b3d380dd4acd3e0613959a8b43bc38
Steven Spencer <stevenistotallyawesome@gmail.com>
2026-01-04 14:33:55 -0500

    forcing sitemap to be dynamic

--- web/src/app/sitemap.tsx
+++ web/src/app/sitemap.tsx
@@ -3,6 +3,8 @@ import { db } from '@/db'
 import { posts, profile } from '@/db/schema'
 import { eq } from 'drizzle-orm'
 
+export const dynamic = 'force-dynamic'
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const baseUrl = 'https://www.tupah.me'
 
