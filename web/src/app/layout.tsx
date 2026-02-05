1d907a72d010ea5295a0ead9f80ced3e9176347d
Steven Spencer <stevenistotallyawesome@gmail.com>
2026-02-05 16:57:04 -0500

    adding google analytics tags

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -2,6 +2,7 @@
 "use client";
 
 import React from "react";
+import Script from "next/script";
 import "@/app/globals.css";
 import Footer from "@/components/Footer/Footer";
 import { AuthProvider } from "@/contexts/AuthContext";
@@ -12,6 +13,21 @@ import { SpeedInsights } from "@vercel/speed-insights/next"
 export default function HomeLayout({ children, }: { children: React.ReactNode; }) {
   return (
     <html lang="en">
+
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-QFVRS8FXBP"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-QFVRS8FXBP');
+          `}
+        </Script>
+      </head>
       <body className="flex flex-col">
         <AuthProvider>
           <main className={`transition-all duration-300 flex-1 overflow-x-hidden overflow-y-auto`}>
